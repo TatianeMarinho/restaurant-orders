0086fce53377eea64e78a719dca17dcebfb29907
TatianeMarinho <devtatianemarinho@gmail.com>
2024-02-19 16:12:26 -0400

    corrigindo test_ingredient.py

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,31 +1,24 @@
-from src.models.ingredient import Ingredient, Restriction, restriction_map  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
     ingredient_one = Ingredient("queijo mussarela")
     expected_restrictions = {
-        Restriction.LACTOSE,
-        Restriction.ANIMAL_DERIVED
+       Restriction.LACTOSE,
+       Restriction.ANIMAL_DERIVED
     }
     assert ingredient_one.name == "queijo mussarela"
     assert ingredient_one.restrictions == expected_restrictions
 
-    ingredient_two = Ingredient("bacon")
-    expected_restrictions_dois = {
-        Restriction.ANIMAL_DERIVED,
-        Restriction.ANIMAL_MEAT
-    }
-    assert ingredient_two.name == "bacon"
-    assert ingredient_two.restrictions == expected_restrictions_dois
-    assert ingredient_one != ingredient_two
+    ingredient_two = Ingredient("alface")
+    ingredient_three = Ingredient("alface")
+    assert ingredient_two.name == "alface"
+    assert ingredient_three.name == "alface"
+    assert ingredient_two.restrictions == set()
+    assert ingredient_two != ingredient_one
+    assert ingredient_two == ingredient_three
+    assert repr(ingredient_two) == "Ingredient('alface')"
     assert repr(ingredient_one) == "Ingredient('queijo mussarela')"
-    assert repr(ingredient_two) == "Ingredient('bacon')"
-    assert hash(ingredient_one) != hash(ingredient_two)
-
-    ingredient_tree = Ingredient("alface")
-    ingredient_four = Ingredient("alface")
-    assert ingredient_tree.name == "alface"
-    assert ingredient_tree.restrictions == set()
-    assert repr(ingredient_tree) == "Ingredient('alface')"
-    assert hash(ingredient_tree) == hash(ingredient_four)
+    assert hash(ingredient_two) == hash(ingredient_three)
+    assert hash(ingredient_two) != hash(ingredient_one)
